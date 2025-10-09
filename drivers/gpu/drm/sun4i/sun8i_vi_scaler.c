32977aa9d120fd1e361c9068182f81dc01163429
Anton Tarasov <ant0@mail.ru>
2025-10-09 15:26:53 +0300

    Revert "drm: sun4i: vi_scaler refactor vi_scaler enablement"
    This reverts commit ed2853c577718333dbb01fadeac49a9f105c115f.

--- drivers/gpu/drm/sun4i/sun8i_vi_scaler.c
+++ drivers/gpu/drm/sun4i/sun8i_vi_scaler.c
@@ -933,13 +933,20 @@ static void sun8i_vi_scaler_set_coeff_ui(struct regmap *map, u32 base,
 			  &table[offset], SUN8I_VI_SCALER_COEFF_COUNT);
 }
 
-void sun8i_vi_scaler_disable(struct sun8i_mixer *mixer, int layer)
+void sun8i_vi_scaler_enable(struct sun8i_mixer *mixer, int layer, bool enable)
 {
-	u32 base;
+	u32 val, base;
 
 	base = sun8i_vi_scaler_base(mixer, layer);
 
-	regmap_write(mixer->engine.regs, SUN8I_SCALER_VSU_CTRL(base), 0);
+	if (enable)
+		val = SUN8I_SCALER_VSU_CTRL_EN |
+		      SUN8I_SCALER_VSU_CTRL_COEFF_RDY;
+	else
+		val = 0;
+
+	regmap_write(mixer->engine.regs,
+		     SUN8I_SCALER_VSU_CTRL(base), val);
 }
 
 void sun8i_vi_scaler_setup(struct sun8i_mixer *mixer, int layer,
@@ -974,9 +981,6 @@ void sun8i_vi_scaler_setup(struct sun8i_mixer *mixer, int layer,
 		chphase = hphase;
 		cvphase = vphase;
 	}
-
-	regmap_write(mixer->engine.regs, SUN8I_SCALER_VSU_CTRL(base),
-		     SUN8I_SCALER_VSU_CTRL_EN);
 
 	if (mixer->cfg->de_type >= sun8i_mixer_de3) {
 		u32 val;
@@ -1023,9 +1027,4 @@ void sun8i_vi_scaler_setup(struct sun8i_mixer *mixer, int layer,
 	else
 		sun8i_vi_scaler_set_coeff_ui(mixer->engine.regs, base,
 					     hscale, vscale, format);
-
-	if (mixer->cfg->de_type <= sun8i_mixer_de3)
-		regmap_write(mixer->engine.regs, SUN8I_SCALER_VSU_CTRL(base),
-			     SUN8I_SCALER_VSU_CTRL_EN |
-			     SUN8I_SCALER_VSU_CTRL_COEFF_RDY);
 }
