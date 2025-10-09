9ebbfef3e75c48c3d8593a342bad6d115ae33495
Anton Tarasov <ant0@mail.ru>
2025-10-09 15:28:59 +0300

    Revert "drm: sun4i: de2/de3: call csc setup also for UI layer"
    This reverts commit b0c6fa51a7fa7bae499067b0eea4231c0ab76b92.

--- drivers/gpu/drm/sun4i/sun8i_csc.c
+++ drivers/gpu/drm/sun4i/sun8i_csc.c
@@ -209,10 +209,8 @@ void sun8i_csc_set_ccsc(struct sun8i_mixer *mixer, int layer,
 		return;
 	}
 
-	if (layer < mixer->cfg->vi_num) {
-		base = ccsc_base[mixer->cfg->ccsc][layer];
+	base = ccsc_base[mixer->cfg->ccsc][layer];
 
-		sun8i_csc_setup(mixer->engine.regs, base,
-				fmt_type, encoding, range);
-	}
+	sun8i_csc_setup(mixer->engine.regs, base,
+			fmt_type, encoding, range);
 }
