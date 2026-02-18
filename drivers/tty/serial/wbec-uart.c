f1eb8c2ef2949d1f80884d87458cbe668354bbd3
Egor Panchenko <egor.panchenko@wirenboard.com>
2026-02-18 17:43:30 +0300

    Small fix to changelog and string formating

--- drivers/tty/serial/wbec-uart.c
+++ drivers/tty/serial/wbec-uart.c
@@ -436,7 +436,7 @@ static void wbec_uart_set_termios(struct uart_port *port, struct ktermios *new,
 	// Older versions will ignore this setting and use 8 data bits.
 	switch (new->c_cflag & CSIZE) {
 	default:
-		dev_err(port->dev, "Failed to set termios: CS value not applied, WBEC-UART supports only CS7 and CS8. 
+		dev_err(port->dev, "Failed to set termios: CS value not applied, WBEC-UART supports only CS7 and CS8. \
 			The default value is set - CS8\n");
 	case CS8:
 		ctrl_regs.ctrl.data_width = 0; /* 8 data bits (default) */
